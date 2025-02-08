7ac2228e4fa963d3e9852f535923f3de9b939f2c
SpaceChihuahua <155734165+SpaceChihuahua@users.noreply.github.com>
2025-02-08 13:44:24 +0200

    homework for lesson 10

---
+++ lesson10/lesson10.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+void swapPtr(int& a, int& b)
+{
+    int* ptrToA = &a;
+    int* ptrToB = &b;
+    int i = a; //store variable 'a' initial value before it is changed
+    *ptrToA = b;
+    *ptrToB = i;
+}
+
+void swapRef(int* a, int* b)
+{
+    int& refToA = *a;
+    int& refToB = *b;
+    int i = refToA; //store variable 'a' initial value before it is changed
+    *a = refToB;
+    *b = i;  
+}
+
+bool find(const int* arr, int size, int elem)
+{
+    const int* endOfArray = arr + size;
+    while (arr < endOfArray)
+    {
+        if (*arr == elem)
+        {
+            return true;
+        }
+        arr++;
+    }
+    return false;
+}
+
+int main()
+{
+    //First swap
+    int a = 1;
+    int b = 2;
+    std::cout << "Variables before swap: \n";
+    std::cout << "Variable a value: " << a << std::endl;
+    std::cout << "Variable a address: " << &a << std::endl;
+    std::cout << "Variable b value: " << b << std::endl;
+    std::cout << "Variable b address: " << &b << std::endl;
+
+    swapPtr(a, b);
+
+    std::cout << "Variables after swap: \n";
+    std::cout << "Variable a value: " << a << std::endl;
+    std::cout << "Variable a address: " << &a << std::endl;
+    std::cout << "Variable b value: " << b << std::endl;
+    std::cout << "Variable b address: " << &b << std::endl;
+
+    //Second swap
+    int c = 3;
+    int d = 4;
+
+    std::cout << "Variables before swap: \n";
+    std::cout << "Variable c value: " << c << std::endl;
+    std::cout << "Variable c address: " << &c << std::endl;
+    std::cout << "Variable d value: " << d << std::endl;
+    std::cout << "Variable d address: " << &d << std::endl;
+    swapRef(&c, &d);
+    std::cout << "Variables after swap: \n";
+    std::cout << "Variable c value: " << c << std::endl;
+    std::cout << "Variable c address: " << &c << std::endl;
+    std::cout << "Variable d value: " << d << std::endl;
+    std::cout << "Variable d address: " << &d << std::endl;
+
+    //pointer arithmetics 
+    const int arraySize = 10;
+    int myArray[arraySize];
+    int elem = 0;
+
+    std::cout << "Enter 10 numbers to fill an array: ";
+    for (int i = 0; i < arraySize; i++)
+    {
+        std::cin >> myArray[i];
+    }
+
+    std::cout << "Enter a number for which we should search within an array: ";
+    std::cin >> elem;
+        
+    int* ptrToMyArray = &myArray[0];
+
+    if (find(ptrToMyArray, arraySize, elem))
+    {
+        std::cout << "Element is found!\n";
+    }
+    else
+    {
+        std::cout << "No such element!\n";
+    }
+
+    return 0;
+
+}
